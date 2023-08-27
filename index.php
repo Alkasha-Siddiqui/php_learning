1901a37a74b5eeecd5d0b10ee346f80f2a33e589
Alkasha Siddiqui <alkashasiddiqui@gmail.com>
2023-08-27 21:24:03 +0530

    Updated index.php file

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
 
-//numArray = [1,1,2,3,3,1,2,4,5,4,6,5,7,7];
-//write a code to find number with less occurance
-
-$numArray = [1,1,2,3,3,1,2,4,5,4,6,5,7,7];
-// $itemCountArray = [];
-
-// $arr = [];
-
-// $arr =['key'] = "name";
-print($numArray[7]);
-
-// foreach ($numArray as $value){
-//     if($itemCountArray [$value]){
-//         $itemCountArray++;
-//     }else{
-//         $itemCountArray[$value] = 1;
-//     }
-// }
-// asort($itemCountArray);
-// $keyArray = array_keys($itemCountArray);
-// print_r($keyArray[0]);
-
-echo "Hello World!";
+$x = 5;
+$y = 10;
+echo $x + $y . "<br>"; // 15
+echo "Hello";
 ?>
